c48e2d5d5180e9364db399a798c625c3c828bb34
João Pereira <jappereira@gmail.com>
2015-04-28 23:57:18 +0100

    Task 2: Calculate total transactions amount for partner

--- src/main/java/com/gamesys/Main.java
+++ src/main/java/com/gamesys/Main.java
@@ -5,7 +5,6 @@ import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -13,32 +12,56 @@ import java.util.stream.Stream;
 
 public class Main {
 
-    private static String EXCHANGERATES_FILENAME;
+    private static String EXCHANGE_RATES_FILENAME;
     private static String TRANSACTIONS_FILENAME;
     private static String PARTNER;
     private static String CURRENCY;
-    private static Map<String, BigDecimal> EXCHANGERATES;
+    private static Map<String, BigDecimal> EXCHANGE_RATES;
 
     public static void main(String[] args) throws IOException {
-        EXCHANGERATES_FILENAME = args[0];
+        EXCHANGE_RATES_FILENAME = args[0];
         TRANSACTIONS_FILENAME = args[1];
         PARTNER = args[2];
         CURRENCY = args[3];
 
-        EXCHANGERATES = loadExchangeRates();
+        EXCHANGE_RATES = loadExchangeRates();
+
+        System.out.println(calculateTotalTransactionsAmountForPartner().get());
     }
 
     private static Map<String, BigDecimal> loadExchangeRates() throws IOException {
         String filter = "," + CURRENCY;
-        Path path = Paths.get(EXCHANGERATES_FILENAME);
-        Map<String, BigDecimal> EXCHANGERATES;
+        Path path = Paths.get(EXCHANGE_RATES_FILENAME);
+        Map<String, BigDecimal> EXCHANGE_RATES;
 
-        try (Stream<String> filteredLines = Files.lines(path).filter(l -> l.startsWith(filter, 3))) {
-            EXCHANGERATES
+        try (Stream<String> filteredLines = Files.lines(path).filter(line -> line.startsWith
+                (filter, 3))) {
+            EXCHANGE_RATES
                     = filteredLines
-                    .collect(Collectors.toMap(e -> e.substring(0, 3), e -> new BigDecimal(e.substring(8))));
+                    .collect(Collectors.toMap(exchangeRate -> exchangeRate.substring(0, 3),
+                            exchangeRate -> new BigDecimal(exchangeRate.substring(8))));
         }
 
-        return EXCHANGERATES;
+        return EXCHANGE_RATES;
+    }
+
+    // Task 2
+    private static Optional<BigDecimal> calculateTotalTransactionsAmountForPartner() throws
+            IOException {
+        Path path = Paths.get(TRANSACTIONS_FILENAME);
+        Optional<BigDecimal> result;
+
+        try (Stream<String> filteredLines = Files.lines(path).filter(l -> l.startsWith(PARTNER))) {
+            result = filteredLines.map((String transaction) -> {
+                String[] strings = transaction.split(",");
+                if (strings[1].equals(CURRENCY)) {
+                    return new BigDecimal(strings[2]);
+                } else {
+                    return new BigDecimal(strings[2]).multiply(EXCHANGE_RATES.get(strings[1]));
+                }
+            }).reduce((a, b) -> a.add(b));
+        }
+
+        return result;
     }
 }
